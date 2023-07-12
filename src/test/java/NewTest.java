1322a59093a2d6493968319ce47079ce1a0803af
Михаил <mimintoff@mail.ru>
2023-07-12 14:06:02 +0300

    change 3 -> 10

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -5,7 +5,8 @@ public class NewTest {
 
     @Test
     void newTest(){
-        int i = 3;
+        int i = 10;
+        System.out.println("This is test");
         Assertions.assertTrue(i > 2);
     }
 }
